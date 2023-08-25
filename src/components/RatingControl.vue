965caccb679abf52646cbe09ab3e322b4796eff7
furkanturgut68 <furkanmehmeturgut@gmail.com>
2023-08-25 11:15:23 +0300

    forms

---
+++ src/components/RatingControl.vue
@@ -0,0 +1,55 @@
+ <template>
+    <ul>
+        <li :class="{activated: modelValue === 'poor'}"><button @click="activated('poor')" type="button">Poor</button></li>
+        <li :class="{activated: modelValue === 'average'}"><button @click="activated('average')" type="button">Average</button></li>
+        <li :class="{activated: modelValue === 'great'}"><button @click="activated('great')" type="button">Great</button></li>
+
+    </ul>    
+ </template>
+
+ <script>
+export default {
+    props : ['modelValue'],
+    emits: ['update:modelValue'],
+   
+   
+    methods: {
+        activated(option){
+            this.$emit('update:modelValue',option);
+           
+        }
+    }
+}
+</script>
+
+<style>
+.activated {
+    border-color: #a00078;
+}
+.activated button {
+    color: #a00078;
+}
+
+ul {
+    display: flex;
+    list-style: none;
+    margin:  0.5rem 0;
+    padding: 0;
+}
+
+li {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1rem;
+    border: 1px solid #ccc;
+    margin: 0 1rem;
+}
+
+button {
+    font: inherit;
+    cursor: pointer;
+    border: none;
+    background-color: transparent;
+}
+</style>
---
+++ src/components/TheForm.vue
@@ -0,0 +1,189 @@
+<template>
+  <form @submit.prevent="submitData">
+    <div class="form-control" :class="{invalid:nameValidity === 'invalid'}">
+      <label for="user-name">Your Name</label>
+      <input id="user-name" name="user-name" type="text" v-model="userName" @blur="inputValid"/>
+      <p v-if="nameValidity === 'invalid'">Please enter a validate!</p>
+    </div>
+    <div class="form-control">
+      <label for="age">Your Age (Years)</label>
+      <input id="age" name="age" type="number" v-model.number="userAge"/>
+    </div>
+    <div class="form-control">
+      <label for="referrer">How did you hear about us?</label>
+      <select id="referrer" name="referrer" v-model="referrer">
+        <option value="google">Google</option>
+        <option value="wom">Word of mouth</option>
+        <option value="newspaper">Newspaper</option>
+      </select>
+    </div>
+    <div class="form-control">
+      <h2>What are you interested in?</h2>
+      <div>
+        <input id="interest-news" name="interest" type="checkbox" value="new" v-model="interest"/>
+        <label for="interest-news">News</label>
+      </div>
+      <div>
+        <input id="interest-tutorials" name="interest" type="checkbox" value="tutorial"  v-model="interest" />
+        <label for="interest-tutorials">Tutorials</label>
+      </div>
+      <div>
+        <input id="interest-nothing" name="interest" type="checkbox" value="nothing" v-model="interest" />
+        <label for="interest-nothing">Nothing</label>
+      </div>
+    </div>
+    <div class="form-control">
+      <h2>How do you learn?</h2>
+      <div>
+        <input id="how-video" name="how"  type="radio" value="video" v-model="how" />
+        <label for="how-video">Video Courses</label>
+      </div>
+      <div>
+        <input id="how-blogs" name="how" type="radio" value="blogs" v-model="how" />
+        <label for="how-blogs">Blogs</label>
+      </div>
+      <div>
+        <input id="how-other" name="how" type="radio" v-model="how"/>
+        <label for="how-other">Other</label>
+      </div>
+    </div>
+
+    <div class="form-control">
+      <rating-control v-model="rating" ></rating-control>
+    </div>
+
+    <div class="form-control">
+      <input type="checkbox" name="terms" id="terms" v-model="confirm">
+      <label>Please an terms</label>
+    </div>
+    <div>
+      <button >Save Data</button>
+    </div>
+  </form>
+</template>
+
+<script>
+import RatingControl from './RatingControl.vue';
+export default {
+  components : {
+    RatingControl
+  },
+  data(){
+    return {
+      userName : '',
+      userAge: null,
+      referrer: 'newspaper',
+      interest: [],
+      how:null,
+      confirm: false,
+      nameValidity: 'pending',
+      rating : null
+    }
+  },
+  methods : {
+    submitData(){
+        console.log('Username: '+this.userName);
+        this.userName = ''
+        // console.log("User age: ");
+        // const myAge= this.userAge;
+        // console.log(typeof(myAge))
+       // console.log(this.nameValidity)
+
+        // console.log(this.userAge);
+        //console.log(this.$refs.ageInput.value)
+        // console.log(32)
+        // console.log("Referrer:" + this.referrer);
+        // this.referrer = 'newspaper'
+        // console.log("Checkboxes");
+        // console.log(this.interest);
+        // console.log("RadiButtons");
+        // console.log(this.how)
+        console.log(this.rating)
+        //console.log(this.nameValidity)
+        this.rating = null
+
+    },
+    inputValid(){
+      if(this.userName.trim() === ''){
+        this.nameValidity = 'invalid'
+      }else {
+        this.nameValidity = 'valid'
+      }
+    }
+  }
+ 
+}
+</script>
+
+
+<style scoped>
+form {
+  margin: 2rem auto;
+  max-width: 40rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 2rem;
+  background-color: #ffffff;
+}
+
+.form-control.invalid input {
+  border-color: red;
+}
+
+.form-control.invalid label {
+  color: red;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+}
+
+h2 {
+  font-size: 1rem;
+  margin: 0.5rem 0;
+}
+
+input,
+select {
+  display: block;
+  width: 100%;
+  font: inherit;
+  margin-top: 0.5rem;
+}
+
+select {
+  width: auto;
+}
+
+input[type='checkbox'],
+input[type='radio'] {
+  display: inline-block;
+  width: auto;
+  margin-right: 1rem;
+}
+
+input[type='checkbox'] + label,
+input[type='radio'] + label {
+  font-weight: normal;
+}
+
+button {
+  font: inherit;
+  border: 1px solid #0076bb;
+  background-color: #0076bb;
+  color: white;
+  cursor: pointer;
+  padding: 0.75rem 2rem;
+  border-radius: 30px;
+}
+
+button:hover,
+button:active {
+  border-color: #002350;
+  background-color: #002350;
+}
+</style>
